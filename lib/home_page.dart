dd5f23c2b055115915afec9d3430ddb0654a72e3
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-09 23:02:31 +0530

    app bar and filters

---
+++ lib/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filter = const ["All", "Addidas", "Nike", "Bata"];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedFilter = filter[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(255, 255, 255, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
+      ),
+    );
+
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "Shoes\nCollection",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filter.length,
+              itemBuilder: (context, index) {
+                final item = filter[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = item;
+                      });
+                    },
+                    child: Chip(
+                      backgroundColor: selectedFilter == item
+                          ? Theme.of(context).colorScheme.primary
+                          : Color.fromRGBO(245, 247, 249, 1),
+                      side: const BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1)),
+                      label: Text(item),
+                      labelStyle: const TextStyle(fontSize: 16),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
