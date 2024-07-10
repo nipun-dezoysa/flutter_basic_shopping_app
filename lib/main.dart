851c5c2857746e024372c46147a0be1e3a1b18d8
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-11 00:15:31 +0530

    product details page finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_basic_shopping_app/home_page.dart';
 
 void main() {
@@ -14,28 +13,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shopping App',
       theme: ThemeData(
-        useMaterial3: false,
+        useMaterial3: true,
         fontFamily: "Lato",
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromRGBO(254, 206, 1, 1),
           primary: const Color.fromRGBO(254, 206, 1, 1),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
         ),
         inputDecorationTheme: const InputDecorationTheme(
           hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
           prefixIconColor: Color.fromRGBO(119, 119, 199, 1),
         ),
         textTheme: const TextTheme(
+          titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
           titleMedium: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 20,
-          ),titleSmall: TextStyle(
+          ),
+          titleSmall: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 16,
           ),
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
