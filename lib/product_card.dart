8f9d35978061bfeb84eab8d38c3ac6efb68819a9
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-10 22:39:25 +0530

    product card bg color

--- lib/product_card.dart
+++ lib/product_card.dart
@@ -4,39 +4,42 @@ class ProductCard extends StatelessWidget {
   final String title;
   final double price;
   final String image;
+  final Color backgroundColor;
   const ProductCard(
       {super.key,
       required this.title,
       required this.price,
-      required this.image});
+      required this.image,
+      required this.backgroundColor,});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(20),
-      color: const Color.fromRGBO(216, 240, 253, 1),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              '\$$price',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Center(child: Image.asset(image, height: 175)),
-          ],
-        ),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: backgroundColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            '\$$price',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Center(child: Image.asset(image, height: 175)),
+        ],
       ),
     );
   }
