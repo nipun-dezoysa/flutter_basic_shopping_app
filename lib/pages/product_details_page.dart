c548427c60509b5a17e50a37b7b867da5a660b3c
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-11 19:38:29 +0530

    responsive layouts addeed

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -54,7 +54,10 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
             const Spacer(),
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset(widget.product['imageUrl'] as String),
+              child: Image.asset(
+                widget.product['imageUrl'] as String,
+                height: 250,
+              ),
             ),
             const Spacer(
               flex: 2,
@@ -104,7 +107,7 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                       onPressed: addToCart,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Theme.of(context).colorScheme.primary,
-                        minimumSize: const Size(double.infinity, 50),
+                        fixedSize: const Size(350, 50),
                       ),
                       child: const Text(
                         "Add to Cart",
